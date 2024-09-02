83a586ab02b395b6f8373aab08f4c82c651709d8
Eliajeremiamhapa <eliamhapa34@gmail.com>
2024-09-02 11:20:08 +0300

    Add files via upload

---
+++ onscreenkeyboard.js
@@ -0,0 +1,37 @@
+function functionClick(){
+    var x = document.getElementById("screen");
+    x.innerHTML =    "Failure is an opportunity to learn";
+}
+
+function functioClick(){
+    var x = document.getElementById("screen");
+    x.innerHTML =    "When the solution is simple god is answering";
+}
+function functioClic(){
+    var x = document.getElementById("screen");
+    x.innerHTML =    " I have no special talent.";;
+}
+function functioCli(){
+    var x = document.getElementById("screen");
+    x.innerHTML =    "I never think of the future. it comes soon enough.";
+}
+function functioCl(){
+    var x = document.getElementById("screen");
+    x.innerHTML =    "Where there is a will there is a way.";
+}
+function functioC(){
+    var x = document.getElementById("screen");
+    x.innerHTML =    "God always take a simplest way.";
+}
+function functio(){
+    var x = document.getElementById("screen");
+    x.innerHTML =    "If the fact don't fit the theory, change the facts.";
+}
+function functi(){
+    var x = document.getElementById("screen");
+    x.innerHTML =    "Once we accept our limit we go beyond them.";
+}
+function funct(){
+    var x = document.getElementById("screen");
+    x.innerHTML =    "Information is not knowledge";
+}
